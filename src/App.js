5351e11697d44819f3c8c8b6c86ed8bca1c0279a
mamlaki <melekredwan@icloud.com>
2023-10-17 01:38:42 -0600

    App.js: Removed icon import that was added by mistake.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { FaXTwitter, FaQuoteLeft, FaGithub, FaGit } from 'react-icons/fa6'
+import { FaXTwitter, FaQuoteLeft, FaGithub } from 'react-icons/fa6'
 import './App.css'
 
 export default function App() {
