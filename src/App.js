6d32ede59507b2ed36dd5e1da7c11380034f00cb
mamlaki <melekredwan@icloud.com>
2023-10-16 23:06:01 -0600

    App.js: Implemented random quote API connection. Also removed unused code.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
+import { useState, useEffect } from 'react'
+
 export default function App() {
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
+  const fetchQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.content)
+        setAuthor(data.author)
+      })
+  }
+
   return (
     <div className='wrapper'>
       <div id='quote-box'>
-        <h1 id='text'>Quote Placeholder</h1>
-        <p id='author'>- Author Placeholder</p>
+        <h1 id='text'>{ quote }</h1>
+        <p id='author'>- { author }</p>
         <button id='new-quote'>New quote</button>
         <a id='tweet-quote' href='/' target='_blank'>Twitter/X Logo Placeholder</a>
       </div>
     </div>
   )
 }
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
